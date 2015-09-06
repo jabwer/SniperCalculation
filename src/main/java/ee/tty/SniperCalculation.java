2d8cb9aced9161e5846df1ab24e88deec5b35461
Ove Kreison <ove.kreison@icefire.ee>
2015-09-06 15:47:32 +0300

    Minor changes to main

--- src/main/java/ee/tty/SniperCalculation.java
+++ src/main/java/ee/tty/SniperCalculation.java
@@ -18,57 +18,58 @@ import java.util.List;
  */
 public class SniperCalculation {
     public static void main(String[] args) {
-        System.out.println("Main class");
+        while(true) {
+            try {
+                String xmlLocation = UtilsClass.getPropValue("situationXml");
 
-        try {
-            String xmlLocation = UtilsClass.getPropValue("situationXml");
-
-            String situationXml = UtilsClass.readXmlFromFile(xmlLocation);
-            SAXParserFactory factory = SAXParserFactory.newInstance();
-            SAXParser saxParser = factory.newSAXParser();
-            SituationHandler handler = new SituationHandler();
-            InputStream inputStream = new ByteArrayInputStream(situationXml.getBytes(Charset.forName("UTF-8")));
-            saxParser.parse(inputStream, handler);
-            List<Situation> situations = handler.situations;
+                String situationXml = UtilsClass.readXmlFromFile(xmlLocation);
+                SAXParserFactory factory = SAXParserFactory.newInstance();
+                SAXParser saxParser = factory.newSAXParser();
+                SituationHandler handler = new SituationHandler();
+                InputStream inputStream = new ByteArrayInputStream(situationXml.getBytes(Charset.forName("UTF-8")));
+                saxParser.parse(inputStream, handler);
+                List<Situation> situations = handler.situations;
 
 
-            xmlLocation = UtilsClass.getPropValue("routingXml");
-            String routingXml = UtilsClass.readXmlFromFile(xmlLocation);
-            RoutingHandler rHandler = new RoutingHandler();
-            inputStream = new ByteArrayInputStream(routingXml.getBytes(Charset.forName("UTF-8")));
-            saxParser.parse(inputStream, rHandler);
-            List<Segment> segments = rHandler.segments;
-            List<Vertex> vertices = rHandler.vertices;
+                xmlLocation = UtilsClass.getPropValue("routingXml");
+                String routingXml = UtilsClass.readXmlFromFile(xmlLocation);
+                RoutingHandler rHandler = new RoutingHandler();
+                inputStream = new ByteArrayInputStream(routingXml.getBytes(Charset.forName("UTF-8")));
+                saxParser.parse(inputStream, rHandler);
+                List<Segment> segments = rHandler.segments;
+                List<Vertex> vertices = rHandler.vertices;
 
-            List<Vertex> changedVertex = new ArrayList<Vertex>();
-            List<Segment> changedSegment = new ArrayList<Segment>();
-            Integer maxThreatLevel = Integer.parseInt(UtilsClass.getPropValue("maxThreatLevel"));
+                List<Vertex> changedVertex = new ArrayList<Vertex>();
+                List<Segment> changedSegment = new ArrayList<Segment>();
+                Integer maxThreatLevel = Integer.parseInt(UtilsClass.getPropValue("maxThreatLevel"));
 
-            for(Situation situation:situations) {
-                situation.setDangerZoneRadius(Integer.parseInt(UtilsClass.getPropValue("sniperRange")));
-                for (Vertex vertex : vertices) {
-                    if (UtilsClass.isInDistanceFromSniper(situation.getLatitude().doubleValue(),
-                            situation.getLongitude().doubleValue(),vertex.getLatY().doubleValue(), vertex.getLonX().doubleValue()) &&
-                            situation.getAltitude().doubleValue() >= vertex.getAltitude().doubleValue()) {
-                        vertex.setThreatLevel(maxThreatLevel);
-                        if(!changedVertex.contains(vertex))
-                            changedVertex.add(vertex);
-                        for(Segment segment:segments) {
-                            System.out.println("Vertex id = " + vertex.getId() + "; segment start=" + segment.getStart() + "; segment end = " + segment.getEnd());
-                            if(segment.getStart().equals(vertex.getId()) || segment.getEnd().equals(vertex.getId())) {
-                                segment.setThreatLevel(maxThreatLevel);
-                                changedSegment.add(segment);
+                for(Situation situation:situations) {
+                    situation.setDangerZoneRadius(Integer.parseInt(UtilsClass.getPropValue("sniperRange")));
+                    for (Vertex vertex : vertices) {
+                        if (UtilsClass.isInDistanceFromSniper(situation.getLatitude().doubleValue(),
+                                situation.getLongitude().doubleValue(),vertex.getLatY().doubleValue(), vertex.getLonX().doubleValue()) &&
+                                situation.getAltitude().doubleValue() >= vertex.getAltitude().doubleValue()) {
+                            vertex.setThreatLevel(maxThreatLevel);
+                            if(!changedVertex.contains(vertex))
+                                changedVertex.add(vertex);
+                            for(Segment segment:segments) {
+                                System.out.println("Vertex id = " + vertex.getId() + "; segment start=" + segment.getStart() + "; segment end = " + segment.getEnd());
+                                if(segment.getStart().equals(vertex.getId()) || segment.getEnd().equals(vertex.getId())) {
+                                    segment.setThreatLevel(maxThreatLevel);
+                                    changedSegment.add(segment);
+                                }
                             }
                         }
                     }
                 }
+
+                SituationChangesWriter.writeSituationChangesXml(situations);
+                RoutingChangesWriter.writeRoutingChangesXml(changedSegment, changedVertex);
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
-            SituationChangesWriter.writeSituationChangesXml(situations);
-            RoutingChangesWriter.writeRoutingChangesXml(changedSegment, changedVertex);
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
